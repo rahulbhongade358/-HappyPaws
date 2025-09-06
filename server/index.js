552adb63ce2dab3a43f173f3844babcdc571b161
rahulbhongade358 <rahulbhongade912@gmail.com>
2025-09-06 14:48:35 +0530

    add mongoose dependency and implement MongoDB connection

--- server/index.js
+++ server/index.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import mongoose from 'mongoose';
 dotenv.config();
  
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const connectDB = async () => {
+    const conn = mongoose.connect(process.env.MONGODB_URL)
+    if(conn){
+        console.log("MongoDB connected successfully ✅");
+    }
+}
+
 app.get('/health',(req,res)=>{
     res.json({status:"OK",message:"Server is healthy"});
 })
 const PORT = process.env.PORT || 8080;
 app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+    console.log(` Server is running on port ${PORT}`);
+    connectDB();
 })
